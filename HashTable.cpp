43d3c05557f10b3c552500c6c18ebeb36c4ba2f7
Jason Smith <jasonsmith7227@gmail.com>
2021-04-17 14:30:22 -0400

    Implemented insertion into hash table

--- HashTable.cpp
+++ HashTable.cpp
@@ -19,3 +19,28 @@ bool HashTable::isEmpty() {
     }
     return true;
 }
+
+// Sums each digit of the date before using modulus with capacity
+int HashTable::hash(int key) {
+    int hash = 0;
+    string keyString = to_string(key);
+    for (int i = 0; i < keyString.length(); i++) {
+        hash += (int) keyString[i];
+    }
+    return hash % capacity;
+}
+
+// Stores conversion rate in table per date
+void HashTable::insert(int key, double value) {
+    int hashedKey = hash(key);
+    // Checks if date is already stored, overwrites if needed
+    auto iter = table[hashedKey].begin();
+    for (iter; iter != table[hashedKey].end(); iter++) {
+        int date = iter->first;
+        if (date == key) {
+            iter->second = value;
+            return;
+        }
+    }
+    table[hashedKey].push_back(make_pair(key, value));
+}
